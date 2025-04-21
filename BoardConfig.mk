301edb84dfa45650a32eda675074adcb408acec6
EdwardWu <bluehome.wu@gmail.com>
2025-04-22 00:05:29 +0800

    BGT_sprout: Setup max brightness
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,7 @@ TW_THEME := portrait_hdpi
 # TW_Y_OFFSET := 115
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_EXCLUDE_APEX := true
+TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1000
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
